d8ed863fc9e46fa0ca4904c615cffd9a179a406b
fazliddin21 <fazliddinabdugaffarov878@gmail.com>
2022-11-27 17:02:16 +0300

    Add html and js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,18 @@
-const userName  = prompt('Ismingizni Kiriting');
-var userStart = confirm('Testni start');
-var test1 = prompt('Bootstrap qachon kim tomonidan asos solingan? '+'\n'+'a - 2011 yil 19 avgust, Mark Otto'+'\n'+'b - 2016-yil 6-sentabr,  Mark'+'\n'+'c - 2011 yil 19 avgust, Mark Otto , Jeykob Tornton');
-var test2 = prompt ('Css bu... '+'\n'+'a - Cascading Style Sheets'+'\n'+'b - Cascading Style Sheets, Dasturlash tili emas '+'\n'+'C-  Style Cascading  Sheets, Dasturlash tili');
-var test3 = prompt('Inline tagni ichida blok tag yozish mumkinmi' + '\n'+'a - xa'+ '\n'+'B - ixtiyoriy'+'\n'+' c - yoq'+'\n');
+var userdoller = (500+250)*11225;
+var userYevro = (120*11685);
+var userNatija = (userdoller + userYevro);
 
-var test4 = prompt('Div bu...'+'\n'+'a - quti'+'\n'+'b - sarlavha tagi'+'\n'+' c - text uchun tag'+'\n');
-var test5 = prompt('Bottomning Nechta type bor?'+'\n'+'A-4'+'\n'+'B-3'+'\n'+'C-2'+'\n');
+const useName = prompt("Ismingizni kiriting");
+var useSumma = prompt("Sizda qancha pul bor'");
 
-console.log( 'Ismingiz'+ ' '+ userName);
 
-console.log(
-  
-    'Testni Boshlaysizmi?'+ ' '+ userStart );
+var elhedding = document.querySelector(".error");
+var elspan = document.querySelector(".finish");
 
-console.log('Bootstrap qachon kim tomonidan asos solingan?'+ '\n'+'Javobingiz -'+' '+ test1+'\n'+'To\'g\'ri Javob-'+' ' +'c');
+if ( useSumma>userNatija ){
+    elhedding.textContent=("Oq yol. Sayohatingiz yaxshi o'tsin");
 
-console.log('Css bu...'+'\n'+'Javobingiz -'+' '+test2+'\n'+'To\'g\'ri Javob-'+' '+'a');
-
-console.log('Inline tagni ichida blok tag yozish mumkinmi'+'\n'+'Javobingiz -'+' '+test3+'\n'+'To\'g\'ri Javob-'+' '+'c');
-
-console.log('Div bu...'+'\n'+'Javobingiz -'+' '+test4+'\n'+'To\'g\'ri Javob-'+' '+'a');
-
-console.log('Bottomning Nechta type bor?'+'\n'+'Javobingiz -'+' '+test5+'\n'+'To\'g\'ri Javob-'+' '+'B');
+} else{
+    elhedding.textContent=("Biroz sabir qilasiz. Pul miqdoringiz kam");
+}
+elspan.textContent=useName
